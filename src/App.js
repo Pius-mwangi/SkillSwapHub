ab076dde800503ef18bec111ce4f4af2cdcda58f
PIUS <relimoringa@gmail.com>
2023-10-07 03:02:26 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Import Routes and Link
 import Login from './Login';
 import SkillSeekerDashboard from './SkillSeekerDashboard';
-import SkillProvider from './SkillProvider'; // Import SkillProvider component
+import SkillProvider from './SkillProvider';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -15,10 +15,35 @@ function App() {
     <Router>
       <div className="App">
         <h1>SkillSwap Hub</h1>
-        <Routes> {/* Use <Routes> to define your routes */}
+        <Routes>
+          {/* Route for the landing page */}
+          <Route
+            path="/"
+            element={
+              !loggedIn ? (
+                <div>
+                  <h2>Welcome to SkillSwap Hub</h2>
+                  <p>Choose your login type:</p>
+                  <div>
+                    <Link to="/login/seeker">Login as Seeker</Link>
+                  </div>
+                  <div>
+                    <Link to="/login/provider">Login as Provider</Link>
+                  </div>
+                </div>
+              ) : (
+                <SkillSeekerDashboard />
+              )
+            }
+          />
+          {/* Route for logging in as a seeker */}
+          <Route path="/login/seeker" element={<Login onLogin={handleLogin} />} />
+          {/* Route for logging in as a provider */}
+          <Route path="/login/provider" element={<Login onLogin={handleLogin} />} />
+          {/* Route for the seeker dashboard */}
           <Route path="/dashboard" element={<SkillSeekerDashboard />} />
-          <Route path="/" element={!loggedIn ? <Login onLogin={handleLogin} /> : <SkillSeekerDashboard />} />
-          <Route path="/skillprovider" element={<SkillProvider />} /> {/* Add a route for SkillProvider */}
+          {/* Route for the provider dashboard */}
+          <Route path="/skillprovider" element={<SkillProvider />} />
         </Routes>
       </div>
     </Router>
